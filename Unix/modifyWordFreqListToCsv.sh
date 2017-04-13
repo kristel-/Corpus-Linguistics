07916c9b246eecf5bdc6ce810dd7934169026fa6
Kristel Uiboaed <kristel.uiboaed@gmail.com>
2017-04-13 12:37:18 +0300

    Update modifyWordFreqListToCsv.sh

--- Unix/modifyWordFreqListToCsv.sh
+++ Unix/modifyWordFreqListToCsv.sh
@@ -6,5 +6,5 @@
 # Modify a word frequency list and
 # sort it by a second column (alphabetically)
 tr -s ' ' \
-| sed 's/^ \([0-9]*\) \([^ ]\)/\1:\2/' \
-| sort -k2 -t: \
+| sed 's/^ *\([0-9]*\) \([^ ]*\)/\1;\2/' \
+| sort -k2 -t; \
